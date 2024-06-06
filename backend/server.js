8cac287d8a8169442e14de18a121327f3cabc049
azmick <118058488+azmick@users.noreply.github.com>
2024-06-06 18:43:37 +0300

    server.js build config.

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,18 @@ app.use(bodyParser.json());
 // Bu satırı ekleyin
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+// React build dosyalarını sunmak için bu satırları ekleyin
+app.use(express.static(path.join(__dirname, "frontend", "build")));
+
 // Route'ları ekleyin
 app.use("/api/users", require("./routes/users"));
 app.use("/api/issues", require("./routes/issues"));
 app.use("/api/articles", require("./routes/articles"));
+
+// React uygulaması için fallback route
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
