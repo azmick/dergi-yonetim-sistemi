416dcd5a28371c3a72213087d3bb0b595d23f211
azmick <118058488+azmick@users.noreply.github.com>
2024-06-06 19:34:38 +0300

    trying deploy.

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,6 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Bu satırı ekleyin
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // React build dosyalarını sunmak için bu satırları ekleyin
