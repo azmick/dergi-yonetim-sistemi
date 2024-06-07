924ef73c20550087cf17709c78e746ae86e976b6
azmick <118058488+azmick@users.noreply.github.com>
2024-06-07 15:59:26 +0300

    add diealect options.

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,7 +9,18 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Eğer SSL sertifikasını doğrulamak istemiyorsanız
+      },
+    },
   }
 );
+
+sequelize
+  .authenticate()
+  .then(() => console.log("PostgreSQL connected"))
+  .catch((err) => console.log("Error: " + err));
 
 module.exports = sequelize;
